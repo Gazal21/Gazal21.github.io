3bb0cba0e9b1ae8219e4daa570a20c32e3d4bda5
Gazal <gazal.gazzall@gmail.com>
2024-11-07 14:58:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.querySelectorAll('.project-row').forEach(row => {
   row.addEventListener('wheel', (e) => {
     e.preventDefault();
     row.scrollBy({
-      left: e.deltaY < 0 ? -100 : 100,
-      behavior: 'smooth'
+      left: e.deltaY < 0 ? -100 : 100, // Scroll left if wheel is scrolled up, right if down
+      behavior: 'smooth' // Smooth scrolling
     });
   });
 });
